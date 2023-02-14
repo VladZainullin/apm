7150204982f5c2d7cb64dfb0db1e44b9ecacdb8a
Vladislav Zainullin <mittoface02@mail.ru>
2023-02-14 18:45:31 +0300

    Инкапсулирует разметку главного компонента

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'pm-root',
-  template: `
-    <div>
-      <h1>{{title}}</h1>
-      <pm-products></pm-products>
-    </div>`
+  templateUrl: 'app.component.html'
 })
 
 export class AppComponent {
