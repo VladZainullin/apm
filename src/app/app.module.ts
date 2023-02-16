93d486c17c1059c5a01cb7e32eacb72412c35193
Vladislav Zainullin <mittoface02@mail.ru>
2023-02-16 15:12:22 +0300

    Добавляет общий модуль

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {ProductListComponent} from "./products/product-list/product-list.component";
 import {FormsModule} from "@angular/forms";
-import {ReplacePipe} from './shared/pipes/replace.pipe';
-import {StarComponent} from './shared/components/star/star.component';
 import {HttpClientModule} from "@angular/common/http";
 import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import { WelcomeComponent } from './home/welcome/welcome.component';
